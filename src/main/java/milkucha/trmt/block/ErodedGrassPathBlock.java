fc79821c977341b8ebd872b64c3564db577702a9
Broekinator <58486857+Broekinator@users.noreply.github.com>
2026-07-08 21:42:47 +0200

    Create ErodedGrassPathBlock.java

---
+++ src/main/java/milkucha/trmt/block/ErodedGrassPathBlock.java
@@ -0,0 +1,35 @@
+package milkucha.trmt.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class ErodedGrassPathBlock extends Block {
+
+    private static final VoxelShape SHAPE = Block.box(0, 0, 0, 16, 15, 16);
+
+    public ErodedGrassPathBlock(Properties settings) {
+        super(settings);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
+        return SHAPE;
+    }
+
+    @Override
+    public void stepOn(ServerLevel world, BlockPos pos, BlockState state, Entity entity) {
+        if (entity instanceof LivingEntity living) {
+            living.addEffect(new MobEffectInstance(MobEffects.SPEED, 20, 0, true, false, false));
+        }
+        super.stepOn(world, pos, state, entity);
+    }
+}
